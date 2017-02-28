5094d75063156c3a42c1f32669b4c4ac519b3d89
Josh-Mergens <bluecatonarock@gmail.com>
2017-02-27 20:22:39 -0800

    better functionality for 'Clear' button.
    'Clear' opens prompt to change grid size.
    
    added buttons to change later change fill style.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 $(document).ready(function() {
-  $(function() {
-
-    for (var i = 0; i < 256; i++) {
-      var $gridBox = $('<div class="gridBox"></div>');
-      $('#gridContainer').append($gridBox);
-    }
-  })
-  $('#gridContainer').hover(function() {
+  var x = 16;
+  var grid = 0;
+  $('#button').mouseup(function() {
+    this.blur()
+  });
+  $('#gridContainer').hover(function fill() {
     $('.gridBox').mouseenter(function() {
       $(this).addClass('fill');
     });
-  })
+  });
+  var gridGen = function() {
+    for (var i = 0; i < x * x; i++) {
+    var $gridBox = $('<div class="gridBox"></div>');
+    $('#gridContainer').append($gridBox);
+    }
+  };
+  var boxSize = function() {
+    if (x < 1 || x > 100 || isNaN(x) == true) {
+      alert('Please enter a value between 1 and 100');
+    }
+      else {
+      var size = 640 / x;
+      $('.gridBox').css('width', size).css('height', size);
+    }
+  };
   $('#button').click(function() {
-    $('.gridBox').removeClass('fill');
-    
-
-  })
+    $('.gridBox').remove();
+    x = prompt('Enter a value between 1 and 100 for x,y dimensions');
+    gridGen();
+    boxSize();
+  });
+  gridGen();
+  boxSize();
 });
